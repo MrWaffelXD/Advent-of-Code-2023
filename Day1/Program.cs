842df63451111bc856a8ca4d0aa52934061675d1
MrWaffelXD <accounts@jonas-heinze.de>
2023-12-01 23:58:44 +0100

    task 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,6 +1,37 @@
 ﻿using System.IO;
+using System.Text.RegularExpressions;
 
 string[] input = File.ReadAllLines("input.txt");
 
+task1(input);
 
-Console.WriteLine("");
+static void task1(string[] input)
+{
+
+    int total = 0;
+
+    foreach (string i in input)
+    {
+        string result = "";
+        string numbers = Regex.Replace(i, "[^0-9]", "");
+
+        List<char> numbersList = numbers.ToCharArray().ToList();
+
+        while (numbersList.Count > 2)
+        {
+            numbersList.RemoveAt(1);
+        }
+
+        if (numbersList.Count == 1)
+        {
+            result = numbersList[0].ToString() + numbersList[0].ToString();
+        }
+        else
+        {
+            result = numbersList[0].ToString() + numbersList[1].ToString();
+        }
+        total += Convert.ToInt32(result);
+    }
+
+    Console.WriteLine(total);
+}
