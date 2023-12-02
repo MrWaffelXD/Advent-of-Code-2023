5de9a3635cb1ea1c7c717f7db355f83f3db7a5c2
MrWaffelXD <accounts@jonas-heinze.de>
2023-12-02 13:33:06 +0100

    improved task 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -15,23 +15,24 @@ static void task1(string[] input)
         string result = "";
         string numbers = Regex.Replace(i, "[^0-9]", "");
 
-        List<char> numbersList = numbers.ToCharArray().ToList();
-
-        while (numbersList.Count > 2)
-        {
-            numbersList.RemoveAt(1);
-        }
-
-        if (numbersList.Count == 1)
-        {
-            result = numbersList[0].ToString() + numbersList[0].ToString();
-        }
-        else
-        {
-            result = numbersList[0].ToString() + numbersList[1].ToString();
-        }
+        result = numbers[0].ToString() + numbers[numbers.Length - 1].ToString();
         total += Convert.ToInt32(result);
     }
 
     Console.WriteLine(total);
+}
+
+static void task2(string[] input)
+{
+    string[] inputReplaced = input.Select(x => x.Replace("one", "1")).ToArray();
+    inputReplaced = input.Select(x => x.Replace("two", "2")).ToArray();
+    inputReplaced = input.Select(x => x.Replace("three", "3")).ToArray();
+    inputReplaced = input.Select(x => x.Replace("four", "4")).ToArray();
+    inputReplaced = input.Select(x => x.Replace("five", "5")).ToArray();
+    inputReplaced = input.Select(x => x.Replace("six", "6")).ToArray();
+    inputReplaced = input.Select(x => x.Replace("seven", "7")).ToArray();
+    inputReplaced = input.Select(x => x.Replace("eight", "8")).ToArray();
+    inputReplaced = input.Select(x => x.Replace("nine", "9")).ToArray();
+
+    Console.WriteLine();
 }
