44117e300618e45fea7049000a681ccfd2a96f8c
MrWaffelXD <accounts@jonas-heinze.de>
2023-12-05 16:55:20 +0100

    day 4 task 1

--- Day4/Day4/Program.cs
+++ Day4/Day4/Program.cs
@@ -14,29 +14,28 @@ namespace Day4
         static void Task1(string[] input)
         {
             int totalScore = 0;
+
             foreach (string card in input)
             {
                 int cardScore = 0;
 
-                string cartRaw = Regex.Replace(card, @"Card {1,}\d{1}:", "");
+                string cartRaw = Regex.Replace(card, @"Card {1,}\d{1,}:", "");
 
                 string[] cartValues = cartRaw.Split('|');
 
-                //int[] winningValues = Regex.Matches(cartValues[0], @"\d{1,}").Cast<Match>().Select(m => Convert.ToInt32(m.Value)).ToArray();
                 List<int> winningValues = new List<int>();
                 List<int> scratchedValues = new List<int>();
 
                 winningValues.AddRange(Regex.Matches(cartValues[0], @"\d{1,}").Cast<Match>().Select(m => Convert.ToInt32(m.Value)).ToArray());
                 scratchedValues.AddRange(Regex.Matches(cartValues[1], @"\d{1,}").Cast<Match>().Select(m => Convert.ToInt32(m.Value)).ToArray());
-                //int[] scratchedValues = Regex.Matches(cartValues[1], @"\d{1,}").Cast<Match>().Select(m => Convert.ToInt32(m.Value)).ToArray();
 
-                foreach(int scratched in scratchedValues)
+                foreach (int scratched in scratchedValues)
                 {
-                    if(winningValues.Find(val => val == scratched) == scratched)
+                    if (winningValues.Find(val => val == scratched) == scratched)
                     {
                         winningValues.Remove(scratched);
 
-                        if(cardScore == 0)
+                        if (cardScore == 0)
                         {
                             cardScore++;
                         }
@@ -50,18 +49,6 @@ namespace Day4
                 totalScore += cardScore;
             }
             Console.WriteLine(totalScore);
-        }
-        static List<int> StringArrayToIntList(string[] array)
-        {
-            List<int> list = new List<int>();
-            foreach (string a in array)
-            {
-                if (a != "")
-                {
-                    list.Add(Convert.ToInt32(a));
-                }
-            }
-            return list;
         }
     }
 }
